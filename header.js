ba31b78f6f2abfa535272ea0fe2ff40bab7af9d3
Sohail Asghar <sohailasghar.workofficial@gmail.com>
2025-09-12 11:43:27 -0700

    services section responsive

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
-  document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.querySelector(".hamburger");
-    const menu = document.querySelector(".menu");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu");
 
-    hamburger.addEventListener("click", () => {
-      console.log("Hamburger clicked ✅"); // test message
-      menu.classList.toggle("show");
-    });
+  hamburger.addEventListener("click", () => {
+    console.log("Hamburger clicked ✅"); // test message
+    menu.classList.toggle("show");
   });
+});
